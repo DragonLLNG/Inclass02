bbe4ef4cf5c92bae27ba0f7ce63eedfb0eb02667
Linh <thung2@uncc.edu>
2022-08-29 23:36:33 -0400

    try

--- app/src/main/java/com/example/group4_inclass02/MainActivity.java
+++ app/src/main/java/com/example/group4_inclass02/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.group4_inclass02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,12 +14,125 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String button1 = getString(R.string.button);
-        String button2 = getString(R.string.button2);
-        String button3 = getString(R.string.button3);
-        String button5 = getString(R.string.button5);
-        String button6 = getString(R.string.button6);
-        String button7 = getString(R.string.button7);
+
+        findViewById(R.id.fivePercent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText priceInput = findViewById(R.id.ticketPrice);
+                String input = priceInput.getText().toString();
+                TextView discountPercent = findViewById(R.id.percent);
+                discountPercent.setText("5%");
+                if (input.isEmpty() || Integer.parseInt(input) < 0) {
+                    // toast error : incorrect value
+                    Toast.makeText(MainActivity.this,"Enter valid price",Toast.LENGTH_LONG).show();
+                }
+                try {
+                    double finalPrice = Double.parseDouble(input)*0.95;
+                    TextView discountPrice = findViewById(R.id.discountPrice);
+                    discountPrice.setText(Double.toString(finalPrice));
+
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        findViewById(R.id.tenPercent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText priceInput = findViewById(R.id.ticketPrice);
+                String input = priceInput.getText().toString();
+                TextView discountPercent = findViewById(R.id.percent);
+                discountPercent.setText("10%");
+                if (input.isEmpty() || Integer.parseInt(input) < 0) {
+                    // toast error : incorrect value
+
+                    return;
+                }
+                try {
+                    double finalPrice = Double.parseDouble(input)*0.90;
+                    TextView discountPrice = findViewById(R.id.discountPrice);
+                    discountPrice.setText(Double.toString(finalPrice));
+
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        findViewById(R.id.fifteenPercent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText priceInput = findViewById(R.id.ticketPrice);
+                String input = priceInput.getText().toString();
+                TextView discountPercent = findViewById(R.id.percent);
+                discountPercent.setText("15%");
+                if (input.isEmpty() || Integer.parseInt(input) < 0) {
+                    // toast error : incorrect value
+
+                    return;
+                }
+                try {
+                    double finalPrice = Double.parseDouble(input)*0.85;
+                    TextView discountPrice = findViewById(R.id.discountPrice);
+                    discountPrice.setText(Double.toString(finalPrice));
+
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        findViewById(R.id.twentyPercent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText priceInput = findViewById(R.id.ticketPrice);
+                String input = priceInput.getText().toString();
+                TextView discountPercent = findViewById(R.id.percent);
+                discountPercent.setText("20%");
+                if (input.isEmpty() || Integer.parseInt(input) < 0) {
+                    // toast error : incorrect value
+
+                    return;
+                }
+                try {
+                    double finalPrice = Double.parseDouble(input)*0.80;
+                    TextView discountPrice = findViewById(R.id.discountPrice);
+                    discountPrice.setText(Double.toString(finalPrice));
+
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        findViewById(R.id.fiftyPercent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText priceInput = findViewById(R.id.ticketPrice);
+                String input = priceInput.getText().toString();
+                TextView discountPercent = findViewById(R.id.percent);
+                discountPercent.setText("50%");
+                if (input.isEmpty() || Integer.parseInt(input) < 0) {
+                    // toast error : incorrect value
+
+                    return;
+                }
+                try {
+                    double finalPrice = Double.parseDouble(input)*0.50;
+                    TextView discountPrice = findViewById(R.id.discountPrice);
+                    discountPrice.setText(Double.toString(finalPrice));
+
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        findViewById(R.id.clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
+
 
 }
