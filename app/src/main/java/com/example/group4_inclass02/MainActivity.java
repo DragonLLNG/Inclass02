9d07525a97ec044d3e1c0935eb9c38c68c624d26
michaelwomack2001 <michaelwomack2001@gmail.com>
2022-08-29 20:51:08 -0400

    more GUI fixes

--- app/src/main/java/com/example/group4_inclass02/MainActivity.java
+++ app/src/main/java/com/example/group4_inclass02/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String button1 = getString(R.string.button);
+        String button2 = getString(R.string.button2);
+        String button3 = getString(R.string.button3);
+        String button5 = getString(R.string.button5);
+        String button6 = getString(R.string.button6);
+        String button7 = getString(R.string.button7);
     }
 }
